cea68d624e872204f53f94137ae7659754e7c8ab
Chn20cs013 <97323829+Chn20cs013@users.noreply.github.com>
2022-01-28 09:23:40 +0700

    Update and rename factorial of a number to factorial of a number.java

--- factorial of a number.java
+++ factorial of a number.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 public class Factorial {
 
     public static void main(String[] args) {
 
-        int num = 10;
+        int num ;
+        Scanner obj=new Scanner(System.in);
+        int num =obj .nextInt();
         long factorial = 1;
         for(int i = 1; i <= num; ++i)
         {
